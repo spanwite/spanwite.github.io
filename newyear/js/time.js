f40775dfd7adcbd18d8b9b6ef7523755e39ab13e
Artem <nerq1337@gmail.com>
2020-12-27 18:34:42 +0300

    Update time.js

--- newyear/js/time.js
+++ newyear/js/time.js
@@ -1,5 +1,5 @@
 document.onkeydown = function (e) {
-	if (e.ctrlKey && (e.keyCode === 67 || e.keyCode === 86 || e.keyCode === 85 || e.keyCode === 117)) {
+	if (e.ctrlKey && (e.keyCode === 86 || e.keyCode === 85 || e.keyCode === 117)) {
 		return false;
 	}
 	return true;
